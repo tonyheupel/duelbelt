283e0e83d451af1edc8ab9cff81798f60f8cec45
Tony Heupel <tonyheupel@gmail.com>
2011-09-17 18:39:01 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duelbelt::Application.config.secret_token = '02abef40f358c88b830386674f634084f4e1255f3286a8f24e67e06fef79b3abb2954a46e9fd1d5a3ede0f0d62eb92f98b7c7a6430fae634d76a6b553d92e023'
