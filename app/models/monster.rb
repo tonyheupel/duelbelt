0ec83a88f6d773c44fb4a8e1c93f241acab10741
Tony Heupel <tonyheupel@gmail.com>
2011-11-09 21:26:30 -0800

    get things lined up

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -9,29 +9,29 @@ class Monster < Card
   belongs_to :attribute,      :inverse_of => nil
   belongs_to :monster_type,   :inverse_of => nil  # Normal, Synchro, Fusion, Ritual
   belongs_to :monster_kind,   :inverse_of => nil  # Warrior, Spellcaster, Fairy, etc.
-  
+
   [:attribute, :monster_type, :monster_kind].each do |field|
     validates_presence_of field
   end
-  
+
   [:attribute, :monster_type, :monster_kind].each do |field|
     validates_associated field
   end
-  
+
   [:attack_points, :defense_points].each do |field|
     validates_presence_of     field
     validates_numericality_of field, :greater_than_or_equal_to => 0    
   end
-  
+
   validates_presence_of     :level
   validates_numericality_of :level, :greater_than_or_equal_to => 1
-  
+
   def effect?
     is_effect
   end
-  
+
   def tuner?
     is_tuner
   end
-  
+
 end
