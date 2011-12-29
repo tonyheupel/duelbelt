7f54eaa1a8307ac141b7f60ecc08b8f4fa7a68d7
Tony Heupel <tonyheupel@gmail.com>
2011-12-29 14:45:16 -0800

    fix trailing whitespace

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,14 +2,14 @@
 class Card
   include Mongoid::Document
   include Mongoid::Slug
-  
+
   field :number,      :type => String
   field :name,        :type => String
   field :description, :type => String
   field :image_url,   :type => String
   field :quantity,    :type => Integer, :default => 1
   field :password,    :type => String  # the number on the VERY bottom-left corner of the card (optional)
-  
+
   # additional items of interest that a card can have "something to do with"
   field :tokens,    :type => Boolean, :default => false
   field :counters,  :type => Boolean, :default => false
@@ -17,17 +17,17 @@ class Card
   field :dice,      :type => Boolean, :default => false
 
   belongs_to :rarity, :inverse_of => nil
-  
+
   [:number, :name, :description].each do |field|
     validates_presence_of field
   end
-  
+
   validates_uniqueness_of :number
 
   validates_numericality_of :quantity, :greater_than_or_equal_to => 0, :only_integer => true
 
   validates_associated :rarity
-  
+
   key  :number # only unique field for a card
   slug :number # ensure url-friendly numbering--this is the only unique field
 end
--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -7,48 +7,48 @@ class MonsterTest < ActiveSupport::TestCase
   setup do
     @types = [Monster, Trap, Spell]
   end
-  
+
   def all_types(&block)
     @types.each do |klass|
       yield klass
     end
   end
-  
+
   def vpo_helper(klass, field, &init_block)
     card = klass.new
     yield card
-    
+
     saved = card.save
     assert !saved, "Saved a #{card.class.name} card without a #{field}"
     assert card.errors.messages.key?(field), "#{card.class.name} doesn't have an error on #{field}"
     assert card.errors.messages[field].include?("can't be blank"), "#{card.class.name} doesn't seem to have a \"can't be blank\" error for #{field}"
   end
-    
+
   test "all cards require a card number" do
     all_types do |klass|
       vpo_helper(klass, :number) do |card|
         card.name = 'name'
         card.description = 'description'
-      end      
-    end  
+      end
+    end
   end
-  
+
   test "all cards require a card name" do
     all_types do |klass|
       vpo_helper(klass, :name) do |card|
         card.number = 'HA-1234'
         card.description = 'description'
-      end      
-    end  
+      end
+    end
   end
-  
+
   test "all cards require a card description" do
     all_types do |klass|
       vpo_helper(klass, :description) do |card|
         card.name = 'name'
         card.number = 'HA-12345'
-      end      
-    end  
+      end
+    end
   end
   
   test "all cards default to quantity of 1" do
@@ -57,7 +57,7 @@ class MonsterTest < ActiveSupport::TestCase
       assert c.quantity == 1, "expect default quantity of 1 for #{c.class.name}"
     end
   end
-  
+
   test "all cards have a minimum quantity of 0" do
     all_types do |klass|
       c = klass.new
@@ -67,12 +67,12 @@ class MonsterTest < ActiveSupport::TestCase
       assert c.errors.messages.include?(:quantity), "Should be an error for quantity"
     end
   end
-  
+
   test "all cards have a quantity field that is an Integer" do
     all_types do |klass|
       c = klass.new
       c.quantity = 0.4
-      
+
       c.name = 'name'
       c.description = 'description'
       c.number = 'number'
@@ -82,5 +82,5 @@ class MonsterTest < ActiveSupport::TestCase
       assert c.errors.messages.include?(:quantity), "#{c.class.name} should have an error on quantity since it's a non-Integer: #{c.inspect}\n#{c.errors}"
     end
   end
-  
+
 end
